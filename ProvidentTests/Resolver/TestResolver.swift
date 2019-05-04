2d234a135516e3a7a89cc9d83b744b755d509bdc
Ceri Hughes <github@cerihughes.co.uk>
2019-05-04 12:12:27 +0100

    Add existing tests and test data

---
+++ ProvidentTests/Resolver/TestResolver.swift
@@ -0,0 +1,31 @@
+//
+//  TestResolver.swift
+//  MadogTests
+//
+//  Created by Ceri Hughes on 02/12/2018.
+//  Copyright © 2019 Ceri Hughes. All rights reserved.
+//
+
+import Foundation
+import Madog
+
+class TestResolver: Resolver<String, Void> {
+    private let testViewControllerProviderCreationFunctions: [() -> ViewControllerProvider<String, Void>]
+    private let testServiceProviderCreationFunctions: [(ServiceProviderCreationContext) -> ServiceProvider]
+
+    init(testViewControllerProviderCreationFunctions: [() -> ViewControllerProvider<String, Void>],
+         testServiceProviderCreationFunctions: [(ServiceProviderCreationContext) -> ServiceProvider]) {
+        self.testViewControllerProviderCreationFunctions = testViewControllerProviderCreationFunctions
+        self.testServiceProviderCreationFunctions = testServiceProviderCreationFunctions
+
+        super.init()
+    }
+
+    override func viewControllerProviderCreationFunctions() -> [() -> ViewControllerProvider<String, Void>] {
+        return testViewControllerProviderCreationFunctions
+    }
+
+    override func serviceProviderCreationFunctions() -> [(ServiceProviderCreationContext) -> ServiceProvider] {
+        return testServiceProviderCreationFunctions
+    }
+}
---
+++ ProvidentTests/TestData.swift
@@ -0,0 +1,47 @@
+//
+//  TestViewControllerProvider.swift
+//  MadogTests
+//
+//  Created by Ceri Hughes on 02/12/2018.
+//  Copyright © 2019 Ceri Hughes. All rights reserved.
+//
+
+import Foundation
+import Madog
+
+class TestViewControllerProvider: ViewControllerProvider<String, Void> {
+    var registered = false, unregistered = false
+    var capturedServiceProviders: [String : ServiceProvider]? = nil
+    override func register(with registry: ViewControllerRegistry<String, Void>) {
+        registered = true
+    }
+    override func unregister(from registry: ViewControllerRegistry<String, Void>) {
+        unregistered = true
+    }
+    override func configure(with serviceProviders: [String : ServiceProvider]) {
+        capturedServiceProviders = serviceProviders
+    }
+}
+
+class TestServiceProvider: ServiceProvider {
+    required init(context: ServiceProviderCreationContext) {
+        super.init(context: context)
+        name = String(describing: TestServiceProvider.self)
+    }
+}
+
+class TestViewControllerProviderFactory {
+    static var created = false
+    static func createViewControllerProvider() -> ViewControllerProvider<String, Void> {
+        created = true
+        return TestViewControllerProvider()
+    }
+}
+
+class TestServiceProviderFactory {
+    static var created = false
+    static func createServiceProvider(context: ServiceProviderCreationContext) -> ServiceProvider {
+        created = true
+        return TestServiceProvider(context: context)
+    }
+}

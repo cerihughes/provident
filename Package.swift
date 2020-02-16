8588abcd11b5fc052dedcd31fa5e381fbde43145
Ceri Hughes <github@cerihughes.co.uk>
2020-02-16 16:43:16 +0000

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "Provident",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(name: "Provident", targets: ["Provident"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Provident",
+            path: "Source"
+        ),
+        .testTarget(
+            name: "ProvidentTests",
+            dependencies: ["Provident"],
+            path: "Tests"
+        ),
+    ]
+)

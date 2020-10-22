db9abb6de10127722c4233a81ddabba74b96f366
Ceri Hughes <github@cerihughes.co.uk>
2020-10-22 16:29:58 +0100

    Add sample skeleton

---
+++ SampleApp/Source/AppDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow()
+        window.rootViewController = UIViewController()
+        window.makeKeyAndVisible()
+
+        self.window = window
+        return true
+    }
+}
